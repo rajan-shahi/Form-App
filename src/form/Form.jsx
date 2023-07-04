a8b5e121833dfad0d073c9d1cf6b92f69612bbe8
rajan <rajanshahi1267@gmail.com>
2023-07-04 09:46:36 -0700

    add Email Adderss button

---
+++ src/form/Form.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+
+export default function Form() {
+  return (
+    <div className=" d-flex  align-items-center min-vh-100  justify-content-center ">
+      <div className=" w-50 border p-5">
+        <div class="">
+          <label for="fullName" class="form-label">
+            Full Name
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            id="fullName"
+            placeholder="Enter Your Full Name"
+          />
+
+          
+<label for="phone-number" class="form-label">
+            Number
+          </label>
+          <input
+            type="number"
+            class="form-control"
+            id="phone-number"
+            placeholder="Enter Your number"
+          />
+
+
+          <label for="email" class="form-label">
+            Email
+          </label>
+          <input
+            type="email"
+            class="form-control"
+            id="email"
+            placeholder="Enter Your Email"
+          />
+
+          <label for="password" class="form-label">
+            Password
+          </label>
+          <input
+            type="password"
+            class="form-control"
+            id="password"
+            placeholder="Enter Your Password"
+          />
+
+
+          <label for="Email-Address" class="form-label">
+            Email Address
+          </label>
+          <input
+            type="number"
+            class="form-control"
+            id="Email-Address"
+            placeholder="Enter Your Email Address"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
