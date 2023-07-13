27aae7ea13365e3a3525d8ea81c6ed18592fc060
rajan <rajanshahi1267@gmail.com>
2023-07-13 01:11:05 -0700

    add

--- src/form/Form.jsx
+++ src/form/Form.jsx
@@ -1,213 +1,53 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function Form() {
+  const [open, setOpen] = useState(false);
+
+  const fun = () => {
+    setOpen(!open);
+  };
+
   return (
-    <div className=" d-flex  align-items-center min-vh-100  justify-content-center ">
-      <div className=" w-50 border p-5">
-        <div class="">
-          <label for="fullName" class="form-label">
-            Full Name
-          </label>
-          <input
-            type="text"
-            class="form-control"
-            id="fullName"
-            placeholder="Enter Your Full Name"
-          />
+    <div className=" d-flex  flex-column gap-5  align-items-center min-vh-100  justify-content-center ">
+      <button onClick={fun} type="button" class="btn btn-primary">
+        Click me
+      </button>
 
-          
-<label for="phone-number" class="form-label">
-            Number
-          </label>
-          <input
-            type="number"
-            class="form-control"
-            id="phone-number"
-            placeholder="Enter Your number"
-          />
+      {open && (
+        <div className=" w-50 border p-5">
+          <div class="">
+            <label for="fullName" class="form-label">
+              Full Name
+            </label>
+            <input
+              type="text"
+              class="form-control"
+              id="fullName"
+              placeholder="Enter Your Full Name"
+            />
 
+            <label for="email" class="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              class="form-control"
+              id="email"
+              placeholder="Enter Your Email"
+            />
 
-          <label for="email" class="form-label">
-            Email
-          </label>
-          <input
-            type="email"
-            class="form-control"
-            id="email"
-            placeholder="Enter Your Email"
-          />
-
-          <label for="password" class="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            class="form-control"
-            id="password"
-            placeholder="Enter Your Password"
-          />
-
-
-          
-          
-<label for="phone-number" class="form-label">
-            Number
-          </label>
-          <input
-            type="number"
-            class="form-control"
-            id="phone-number"
-            placeholder="Enter Your number"
-          />
-
-
-          <label for="email" class="form-label">
-            Email
-          </label>
-          <input
-            type="email"
-            class="form-control"
-            id="email"
-            placeholder="Enter Your Email"
-          />
-
-          <label for="password" class="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            class="form-control"
-            id="password"
-            placeholder="Enter Your Password"
-          />
-
-
-          <label for="Email-Address" class="form-label">
-            Email Address
-          </label>
-          <input
-            type="number"
-            class="form-control"
-            id="Email-Address"
-            placeholder="Enter Your Email Address"
-          />
-
-          
-<label for="password" class="form-label">
-            Password
-          </label>
-          <input
-            type="password"
-            class="form-control"
-            id="password"
-            placeholder="Enter Your Password"
-          />
-
-
-          <label for="Email-Address" class="form-label">
-            Email Address
-          </label>
-          <input
-            type="number"
-            class="form-control"
-            id="Email-Address"
-            placeholder="Enter Your Email Address"
-          />
-
-          
-<label for="Email-Address" class="form-label">
-            Email Address
-          </label>
-          <input
-            type="number"
-            class="form-control"
-            id="Email-Address"
-            placeholder="Enter Your Email Address"
-          />
-         
-          
-         <label for="Email-Address" class="form-label">
-            Email Address
-          </label>
-          <input
-            type="number"
-            class="form-control"
-            id="Email-Address"
-            placeholder="Enter Your Email Address"
-          />
-           
-           <label for="Email-Address" class="form-label">
-            Email Address
-          </label>
-          <input
-            type="number"
-            class="form-control"
-            id="Email-Address"
-            placeholder="Enter Your Email Address"
-          />
-
-          
-<label for="Email-Address" class="form-label">
-            Email Address
-          </label>
-          <input
-            type="number"
-            class="form-control"
-            id="Email-Address"
-            placeholder="Enter Your Email Address"
-          />
-          
-          <label for="Email-Address" class="form-label">
-            Email Address
-          </label>
-          <input
-            type="number"
-            class="form-control"
-            id="Email-Address"
-            placeholder="Enter Your Email Address"
-          />
-          
-          <label for="Email-Address" class="form-label">
-            Email Address
-          </label>
-          <input
-            type="number"
-            class="form-control"
-            id="Email-Address"
-            placeholder="Enter Your Email Address"
-          />
-          
-          <label for="Email-Address" class="form-label">
-            Email Address
-          </label>
-          <input
-            type="number"
-            class="form-control"
-            id="Email-Address"
-            placeholder="Enter Your Email Address"
-          />
-          
-          <label for="Email-Address" class="form-label">
-            Email Address
-          </label>
-          <input
-            type="number"
-            class="form-control"
-            id="Email-Address"
-            placeholder="Enter Your Email Address"
-          />
-          
-          <label for="Email-Address" class="form-label">
-            Email Address
-          </label>
-          <input
-            type="number"
-            class="form-control"
-            id="Email-Address"
-            placeholder="Enter Your Email Address"
-          />
+            <label for="password" class="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              class="form-control"
+              id="password"
+              placeholder="Enter Your Password"
+            />
+          </div>
         </div>
-      </div>
+      )}
     </div>
   );
 }
